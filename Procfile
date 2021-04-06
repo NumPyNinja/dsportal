67a7d5ac8db7d1813749a3ff85127a90ed2fae87
Revathy-Krishna <revathykrishna.nair@gmail.com>
2021-04-05 22:31:44 -0400

    Installed heroku and added Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn dsmodule.wsgi
--- dsmodule/settings.py
+++ dsmodule/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import environ
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,3 +135,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 print(os.path.join(BASE_DIR))
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
+
+# adding django heroku
+django_heroku.settings(locals())
